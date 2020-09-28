fdc53813b8d4a4df0d4c844f31ade43cc7e7f40b
Luke <lukecharles222@gmail.com>
2020-09-28 16:06:11 +0100

    Add files via upload

---
+++ port_scanner_simple.py
@@ -0,0 +1,15 @@
+import socket
+
+ip = '192.168.0.49'
+port_list = [20, 80, 8080, 139, 445, 23, 21, 22]
+
+for port in port_list:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = s.connect_ex((ip, port))
+
+    if result == 0:
+        print('=' * 60)
+        print('port ', port, ' open')
+        print('=' * 60)
+    else:
+        print('port ', port, ' closed')
